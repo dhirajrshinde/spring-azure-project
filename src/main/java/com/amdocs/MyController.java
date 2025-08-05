6f21a294b59d056dbf59fde71a5a63cbe6f48739
Dhiraj Shinde <dhirajrshinde@github.com>
2025-08-05 12:26:26 +0530

    first commit

---
+++ src/main/java/com/amdocs/MyController.java
@@ -0,0 +1,12 @@
+package com.amdocs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class MyController {
+
+	@GetMapping("/")
+	public String  Home() {
+		return "Welcome to Jenkins Demo";
+	}
+}
