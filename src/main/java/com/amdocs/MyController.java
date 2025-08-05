9194e315ca325a3f6c3da3105e58040131e0bccf
Dhiraj Shinde <dhirajrshinde@github.com>
2025-08-05 12:59:33 +0530

    2nd

--- src/main/java/com/amdocs/MyController.java
+++ src/main/java/com/amdocs/MyController.java
@@ -9,4 +9,8 @@ public class MyController {
 	public String  Home() {
 		return "Welcome to Jenkins Demo";
 	}
+	@GetMapping("/home")
+	public String  Home2() {
+		return "Welcome to Jenkins Demo 2";
+	}
 }
